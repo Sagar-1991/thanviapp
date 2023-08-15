ae64addd3abf3de875e9e9d6c4334ccd4106210d
Sagar-1991 <veerappagaris@gmail.com>
2023-08-15 12:03:41 +0530

    fk1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {useFormik} from 'formik'
+import * as Yup from 'yup';
 
 function App() {
+ var myform= useFormik({
+    initialValues:{
+     firstname:'',
+     email:''
+    },
+    validationSchema: Yup.object({
+      firstname: Yup.string().required('konchem chusukooo'),
+      email:Yup.string().email().required('sarigga enter cheyy..'), 
+    }),
+    onSubmit:(x)=>{
+      console.log(x)
+    }
+  })
   return (
-    <div>App</div>
-  )
+    <div style={{border:'2px solid blue',padding:'10px',margin:'10px'}}>
+      <h1>Formik practical class</h1>
+      <div style={{border:'2px solid green',padding:'10px',margin:'10px'}}>
+       <form onSubmit={myform.handleSubmit}>
+        <input type="text" name='firstname' onChange={myform.handleChange}/>
+        <br />
+        <input type="text" name='email' onChange={myform.handleChange} />
+        <br />
+        {JSON.stringify(myform.errors)}
+        <button type='submit'>Save</button>
+       </form>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
