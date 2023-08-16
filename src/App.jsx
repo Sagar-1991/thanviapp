5de84d8c936af0f0d061b8c018b58bf8350b48e9
Sagar-1991 <veerappagaris@gmail.com>
2023-08-16 18:01:34 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Aboutus from './Aboutus';
+import Contactus from './contactus'
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>This is App</h1>
+   
+
+      <Outlet></Outlet>
+      <Aboutus></Aboutus>
+      <Contactus></Contactus>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Contactus from './contactus';
+import Aboutus from './Aboutus';
+var router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App></App>,
+        children:[
+
+            {
+                path:"/contactus",
+                element:<Contactus></Contactus>
+            },
+            {
+                path:"/Aboutus",
+                element:<Aboutus></Aboutus>
+            }
+        ]
+    },
+    
+]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
